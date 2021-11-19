2c3ddfc96f8e3358af6ef21da9645431a01752ba
Albert Takács <albitakacs@gmail.com>
2021-11-19 16:03:50 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './sass/main.scss';
 import axios from "axios";
 
+const AUTH_TOKEN = "24370619-9679c13b7b313e4f61ec3203f";
 axios.defaults.baseURL = 'https://pixabay.com/api/';
+axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+
