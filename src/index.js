8609c1ba2c608ccec9c51636939fff6f865552f2
Albert Takács <albitakacs@gmail.com>
2021-11-28 11:12:10 +0100

    fixed gallery card, add adaptive layout

--- src/index.js
+++ src/index.js
@@ -61,29 +61,29 @@ async function onSearchImg(event){
 function renderCardImg(elements){
 return    elements.map(({ webformatURL, tags, likes, views, comments, downloads, largeImageURL }) => {
 
-        return `<a class="gallery-link" href="${largeImageURL}">
-        <div class="photo-card">
-        <img class="photo-card_img"   src="${webformatURL}" alt="${tags}" loading="lazy" />
-        <div class="info">
-          <p class="info-item">
-            <b>Likes</b>
-            ${likes}
-          </p>
-          <p class="info-item">
-            <b>Views</b>
-            ${views}
-          </p>
-          <p class="info-item">
-            <b>Comments</b>
-            ${comments}
-          </p>
-          <p class="info-item">
-            <b>Downloads</b>
-            ${downloads}
-          </p>
-        </div>
-      </div>
-      </a>`
+        return `<div class="photo-card">
+        <a class="gallery-link" href="${largeImageURL}">
+          <img class="photo-card_img"   src="${webformatURL}" alt="${tags}" loading="lazy" />
+          <div class="info">
+            <p class="info-item">
+              <b>Likes</b>
+              ${likes}
+            </p>
+            <p class="info-item">
+              <b>Views</b>
+              ${views}
+            </p>
+            <p class="info-item">
+              <b>Comments</b>
+              ${comments}
+            </p>
+            <p class="info-item">
+              <b>Downloads</b>
+              ${downloads}
+            </p>
+          </div>
+        </a>
+      </div>`
     }).join('');
 }
 
